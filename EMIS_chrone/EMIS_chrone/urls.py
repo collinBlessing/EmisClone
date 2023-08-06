4f447b5ad8537119c2b34ef7523c44328f5ed0fc
Nahurira Collin Blessing <135744501+collinBlessing@users.noreply.github.com>
2023-08-07 00:08:04 +0300

    converted all fles to django format

--- EMIS_chrone/EMIS_chrone/urls.py
+++ EMIS_chrone/EMIS_chrone/urls.py
@@ -18,9 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 import login.urls
 import register.urls
+import dashboard.urls
+import dataupload.urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(login.urls)),
     path('register/', include(register.urls)),
+    path('dashboard/', include(dashboard.urls)),
+    path('emisdataupload/', include(dataupload.urls)),
 ]
