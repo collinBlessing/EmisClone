09f2654e825d36abe6579b7d7c46df935db71f31
Nahurira Collin Blessing <135744501+collinBlessing@users.noreply.github.com>
2023-08-08 15:08:09 +0300

    working register in django

--- EMIS_chrone/register/admin.py
+++ EMIS_chrone/register/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Users
+from .models import School
 # Register your models here.
-admin.site.register(Users)
+admin.site.register(School)
--- EMIS_chrone/register/urls.py
+++ EMIS_chrone/register/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.register, name='register')
+    path('', views.register, name='register'),
+    path('registerUser', views.register_user_view, name='registerUser'),
 
 ]
